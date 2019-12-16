e8a73c7b6929d8edcd14f43706acdc1e2c896958
deep-thinker13 <44936975+deep-thinker13@users.noreply.github.com>
2019-12-16 20:56:35 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+    $servername = "localhost";
+    $username = "id11636272_root";
+    $password = "society@123#";
+    $dbname = "id11636272_society";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT username,password FROM users WHERE username = :username";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':username',$uname);
+        $uname = $_POST['username'];
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            if(!($r = $stmt->fetch())) {
+                echo "User doesn't exist/0";
+            }
+            else{
+                if($_POST['password'] === $r['password'])
+                {
+                    echo "Successfully signed in/1";
+                }
+                else
+                {
+                    echo "Incorrect Password!/0";
+                }
+            }
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+    
+    $conn = null;
+    ?>
---
+++ signup.php
@@ -0,0 +1,43 @@
+<html>
+<body>
+    <?php
+    $servername = "localhost";
+    $username = "id11636272_root";
+    $password = "society@123#";
+    $dbname = "id11636272_society";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "SELECT name FROM users WHERE username = :username OR email = :email";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':username',$uname);
+        $stmt->bindParam(':email',$email);
+
+        $uname = $_POST['username'];
+        $email = $_POST['email'];
+        $stmt->execute();
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+            if(!($r = $stmt->fetch())) {
+                $sql = "INSERT INTO users(name,email,username,password)
+                VALUES('{$_POST["Name"]}','{$_POST["email"]}','{$_POST["username"]}','{$_POST["password"]}')";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                echo "<script>alert(\"Signed Up successfully\")</script>";
+                echo "<meta http-equiv=\"refresh\" content=\"1;index.html\">";
+            }
+            else{
+                echo "<script>alert(\"given user {$r['name']} already exists\")</script>";
+                echo "<meta http-equiv=\"refresh\" content=\"1;signup.html\">";
+            }
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+    
+    $conn = null;
+    ?>
+</body>
+</html>
